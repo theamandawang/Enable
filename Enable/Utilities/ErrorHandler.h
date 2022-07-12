73f6cb78774a5a520884eb7b504d1bb4dc3fef8d
Amanda Wang <amandawang@fb.com>
2022-07-12 15:08:45 -0700

    Add notes for network reachabillity

--- Enable/Utilities/ErrorHandler.h
+++ Enable/Utilities/ErrorHandler.h
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 NS_ASSUME_NONNULL_BEGIN
-
+//TODO: handle errors with network connection https://stackoverflow.com/questions/1083701/how-can-i-check-for-an-active-internet-connection-on-ios-or-macos
 @interface ErrorHandler : NSObject
 + (void) showAlertFromViewController: (UIViewController* _Nonnull)vc title: (NSString *) title message: (NSString * _Nonnull) message completion: (void (^ _Nonnull)(void))completion;
 @end
